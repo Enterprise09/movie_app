e5293683337ca391c37cea1516a7f54ae41ac17b
Enterprise09 <goldenstaytes@gmail.com>
2021-06-26 23:40:28 +0900

    Get Array by map _ function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h3>I like {fav}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://shop.hansalim.or.kr/im/is/activeDesigner/100101004_content1.jpg",
+  },
+  {
+    name: "Camera",
+    image:
+      "https://mblogthumb-phinf.pstatic.net/20160710_288/fmamlove_1468129860896x88U9_PNG/5dmark4.png?type=w800",
+  },
+  {
+    name: "MacBook",
+    image:
+      "https://www.techadvisor.com/cmsdata/features/3802413/new-apple-macbook-air-2021-release-date-price-spec-rumours-main_thumb1200_16-9.png",
+  },
+  {
+    name: "IMac",
+    image:
+      "https://www.apple.com/newsroom/images/product/imac/standard/apple_new-imac-spring21_hero_04202021_Full-Bleed-Image.jpg.large.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Enterprise</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="spageti" />
-      <Food fav="wine" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
