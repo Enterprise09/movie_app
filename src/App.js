764e75978fe6ad2831d1dd95845fb4a8b9688a51
Enterprise09 <goldenstaytes@gmail.com>
2021-06-26 17:42:24 +0900

    What is Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Potato from "./potato";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!!</h1>
+      <h1>Enterprise</h1>
+      <Potato />
     </div>
   );
 }
