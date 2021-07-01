aaef62564ead0b3254bb8ec92161967b321a4f16
Enterprise09 <goldenstaytes@gmail.com>
2021-07-01 13:46:58 +0900

    use current parameter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
     count: 0,
   };
   add = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState((current) => ({ count: current.count + 1 }));
   };
   minus = () => {
-    this.setState({ count: this.state.count - 1 });
-  }; //minus
+    this.setState((current) => ({ count: this.state.count - 1 }));
+  };
   render() {
     return (
       <div>
