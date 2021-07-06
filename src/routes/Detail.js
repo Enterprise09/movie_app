ee36c880bd14b3161187ef6ca10dcfe24c74fd1a
Enterprise09 <goldenstaytes@gmail.com>
2021-07-07 00:06:54 +0900

    Design Detail Page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -8,10 +9,27 @@ class Detail extends React.Component {
       history.push("/");
     }
   }
+  //<span>{location.state.title}</span>;
   render() {
     const { location } = this.props;
+    const { year, title, summary, poster, genres } = location.state;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail_box">
+          <img src={poster} />
+          <h2 className="title_box">{title}</h2>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="li_genres">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p>
+            <p className="summary_box"> {summary} </p>
+          </p>
+        </div>
+      );
     } else {
       return null;
     }
